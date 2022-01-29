68303f79d0200b1c095dd0085599db530143c9de
Juhee Lee <juhee971204@naver.com>
2022-01-29 23:16:23 +0900

    python 자료형

--- prac_python/hello.py
+++ prac_python/hello.py
@@ -1,8 +1,30 @@
 print('Hello, jungle')
+
+# 변수 선언
 a=3
 print(a)
 b=a
 print(b)
 a=5
 print(a,b)
-a                                           
+a
+
+# 자료형과 기본 연산
+# 숫자형
+a=3.8
+a = 7
+b = 2
+print(a/b) #몫
+print(a//b) #몫 (소수점 제거)
+print(a**b) # 거듭제곱
+# 증감연산자 없음
+
+
+# 문자형
+a="3"
+#print(a+5) #문자열과 숫자형 더하기 불가능
+print(a*5) #33333
+
+myname = 'jungle'
+print(myname.upper()) #대문자로
+
