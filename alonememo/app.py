1ba557e7de20555316484ee5f9574b04d1cd8ace
Juhee Lee <juhee971204@naver.com>
2022-02-05 18:10:15 +0900

    jungleSW TEST

--- alonememo/app.py
+++ alonememo/app.py
@@ -1,31 +1,50 @@
 from flask import Flask, render_template, jsonify, request
-app = Flask(__name__)
-
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient  
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbjungle
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)  
+db = client.dbsparta  
 
-## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/memo', methods=['POST'])
+def post_article():
+    url_receive = request.form['url_give']  
+    comment_receive = request.form['comment_give']  
+    article = {'url': url_receive, 'comment': comment_receive}
+    db.articles.insert_one(article)
+    return jsonify({'result': 'success'})
 
 @app.route('/memo', methods=['GET'])
-def listing():
-    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
-    # 2. articles라는 키 값으로 영화정보 내려주기
-    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!'})
+def read_articles():
+    result = list(db.articles.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'articles': result})
 
-## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def saving():
-		# 1. 클라이언트로부터 데이터를 받기
-		# 2. meta tag를 스크래핑하기
-		# 3. mongoDB에 데이터 넣기
-    return jsonify({'result': 'success', 'msg':'POST 연결되었습니다!'})
+@app.route('/api/update', methods=['POST'])
+def get_memo():
+    name_receive = request.form['name_give']
+    memo = db.articles.find_one({'url': name_receive})
+    origin_title = memo['url']
+    origin_content = memo['comment']
+    return jsonify({'result': 'success', 'origin_title': origin_title, 'origin_content': origin_content})
+
+@app.route('/api/like', methods=['POST'])
+def save_memo():
+    recieve_title = request.form['new_title']
+    recieve_content = request.form['new_content']
+    recieve_origin = request.form['origin']
+    db.articles.update_one({'url':  recieve_origin}, {'$set': {'url':recieve_title, 'comment':recieve_content}})
+    return jsonify({'result': 'success'})
+
+@app.route('/api/delete', methods=['POST'])
+def delete_memo():
+    name_receive = request.form['name_give']
+    db.articles.delete_one({'url': name_receive})
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
